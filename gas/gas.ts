951c7b42a4b2e80cef1ffdd298caa18fe9b96c7d
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-26 01:48:01 +0800

    code clean

--- gas/gas.ts
+++ gas/gas.ts
@@ -1,22 +1,17 @@
-const Gas = (function () {
-  const VERSION = '0.1.0'
+const MAX_EXECUTION_TIME = 6 * 60 * 1000 // 6 mins - https://developers.google.com/apps-script/guides/services/quotas#current_quotas
+const MAX_LIFE_TIME      = MAX_EXECUTION_TIME / 6
 
-  var MAX_EXECUTION_TIME = 6 * 60 * 1000 // 6 mins - https://developers.google.com/apps-script/guides/services/quotas#current_quotas
-  var MAX_LIFE_TIME      = MAX_EXECUTION_TIME / 6
+const START_TIME = (new Date()).getTime()
 
-  var START_TIME = (new Date()).getTime()
+class Gas {
 
-  return {
-    getLifeTime:      getLifeTime
-    , getLifeSeconds: function () { return Math.floor(getLifeTime() / 1000) }
-
-    , isYourTime: function () { return getLifeTime() > MAX_LIFE_TIME }
-  }
-
-
-  ///////////////////////////////////////////////////////////
-
-  function getLifeTime() {
+  static getLifeTime () {
     return (new Date()).getTime() - START_TIME
   }
-}())
+
+  static getLifeSeconds () { return Math.floor(this.getLifeTime() / 1000) }
+  static isYourTime () { return this.getLifeTime() > MAX_LIFE_TIME }
+
+}
+
+export { Gas }
